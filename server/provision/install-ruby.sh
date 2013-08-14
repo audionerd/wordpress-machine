bd9f83ab08dd55e2abe4d42b731a376ab234316a
Eric Skogen <eric.skogen@space150.com>
2013-08-14 10:22:41 -0500

    indentation fix

--- server/provision/install-ruby.sh
+++ server/provision/install-ruby.sh
@@ -6,40 +6,40 @@ if [ ! -d /usr/local/rbenv ]; then
     #
     apt-get install make
     apt-get install libssl-dev
-	apt-get install libreadline-dev
-	apt-get install zlib1g-dev
+    apt-get install libreadline-dev
+    apt-get install zlib1g-dev
 
     echo "Installing rbenv"
     #
-	cd /usr/local
-	git clone git://github.com/sstephenson/rbenv.git rbenv
-	chgrp -R staff rbenv
-	chmod -R g+rwxXs rbenv
+    cd /usr/local
+    git clone git://github.com/sstephenson/rbenv.git rbenv
+    chgrp -R staff rbenv
+    chmod -R g+rwxXs rbenv
 
     echo "Adding rbenv init to /home/vagrant/.profile"
     #
-	echo 'export RBENV_ROOT=/usr/local/rbenv' >> /home/vagrant/.profile
-	echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /home/vagrant/.profile
-	echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
+    echo 'export RBENV_ROOT=/usr/local/rbenv' >> /home/vagrant/.profile
+    echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /home/vagrant/.profile
+    echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
 
     echo "Adding rbenv init to /root/.profile"
-	echo 'export RBENV_ROOT=/usr/local/rbenv' >> /root/.profile
-	echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /root/.profile
-	echo 'eval "$(rbenv init -)"' >> /root/.profile
-	source /root/.profile
+    echo 'export RBENV_ROOT=/usr/local/rbenv' >> /root/.profile
+    echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /root/.profile
+    echo 'eval "$(rbenv init -)"' >> /root/.profile
+    source /root/.profile
 
-	echo "Installing ruby-build"
-	#
-	cd /usr/local/rbenv
-	mkdir plugins
-	cd plugins
-	git clone git://github.com/sstephenson/ruby-build.git
-	chgrp -R staff ruby-build
-	chmod -R g+rwxs ruby-build
+    echo "Installing ruby-build"
+    #
+    cd /usr/local/rbenv
+    mkdir plugins
+    cd plugins
+    git clone git://github.com/sstephenson/ruby-build.git
+    chgrp -R staff ruby-build
+    chmod -R g+rwxs ruby-build
 
-	echo "Installing Ruby 2.0.0-p0"
-	rbenv install 2.0.0-p0
+    echo "Installing Ruby 2.0.0-p0"
+    rbenv install 2.0.0-p0
 
-	echo "Setting rbenv global version to Ruby 2.0.0-p0"
-	rbenv global 2.0.0-p0
+    echo "Setting rbenv global version to Ruby 2.0.0-p0"
+    rbenv global 2.0.0-p0
 fi
